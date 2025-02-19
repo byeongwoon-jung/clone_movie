923bed8a12e348552869e33dee131d61e69c7f85
bwjung <cholei2236@gmail.com>
2025-02-19 19:57:51 +0900

    rating fix

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,7 +8,7 @@ function Home() {
   // 영화 정보가져오기
   const getMovies = async () => {
     const response = await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8&sort_by=year"
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
     );
 
     const json = await response.json();
